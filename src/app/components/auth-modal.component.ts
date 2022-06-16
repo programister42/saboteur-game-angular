e514bbdda87847db662f4e769cde5f5602c5376e
Taras Tereschenko <tarrrasio@gmail.com>
2022-06-16 11:14:51 +0500

    [2pcuj4k] Refactor auth service to use json

--- src/app/components/auth-modal.component.ts
+++ src/app/components/auth-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, Validators } from '@angular/forms'
+import { Observable } from 'rxjs'
 import { AuthorizationService } from '../services/authorization.service'
 
 @Component({
@@ -11,9 +12,11 @@ import { AuthorizationService } from '../services/authorization.service'
         <mat-card-subtitle>Регистрация</mat-card-subtitle>
         <mat-card-title>Придумайте никнейм</mat-card-title>
         <mat-card-content>
-          <mat-form-field class="w-full" appearance="standard">
+          <mat-form-field class="w-full" appearance="outline">
             <mat-label>Ваш ник</mat-label>
             <input matInput id="name" type="text" [formControl]="name">
+            <mat-hint class="mat-error" *ngIf="serverError">{{ serverError }}</mat-hint>
+            <mat-hint>Никнейм должен быть не менее 3-х и не более 20-ти символов</mat-hint>
           </mat-form-field>
         </mat-card-content>
         <mat-divider inset></mat-divider>
@@ -35,10 +38,15 @@ export class AuthModalComponent implements OnInit {
     Validators.maxLength(20),
   ])
   progressBarMode: 'determinate' | 'indeterminate' = 'determinate'
+  serverError: string = ''
 
   constructor(private authService: AuthorizationService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.authService.serverError$.subscribe((error) => {
+      this.serverError = error
+    })
+  }
 
   signUp() {
     if (this.name.valid) {
